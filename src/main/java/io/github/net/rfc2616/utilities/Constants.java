24522f3308e802764ac5e4da481510aeae8cc259
Claudiney Nascimento <contato@claudiney.info>
2022-09-10 17:07:31 -0300

    Add class for constants

---
+++ src/main/java/io/github/net/rfc2616/utilities/Constants.java
@@ -0,0 +1,10 @@
+package io.github.net.rfc2616.utilities;
+
+public final class Constants {
+
+	public static final String ENV_PORT = "io.github.net.rfc2616.server.port";
+	public static final String ENV_HOSTNAME = "io.github.net.rfc2616.server.hostname";
+
+	private Constants() { /***/ }
+
+}
